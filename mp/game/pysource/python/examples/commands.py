66440a312260c6520439dab27367d6cfe6c731de
Sandern <vannoortsander@gmail.com>
2013-08-04 16:15:49 +0200

    Added my_model_entity example from VDC, implemented in Python. Added a test command for destroying all my_model_entity instances, showing what happens to the handle and instance refs when destroyed (py_example_mymodelentity_removeall).

--- mp/game/pysource/python/examples/commands.py
+++ mp/game/pysource/python/examples/commands.py
@@ -1,6 +1,9 @@
-from gameinterface import concommand
-from entities import CreateEntityByName, DispatchSpawn, eventqueue, variant_t
-from utils import UTIL_GetCommandClient
+from vmath import Vector, QAngle, AngleVectors
+from gameinterface import concommand, engine
+from entities import CreateEntityByName, DispatchSpawn, eventqueue, variant_t, entlist
+from utils import UTIL_GetCommandClient, UTIL_RemoveImmediate
+
+import traceback
 
 print('Registering examples package commands. Use "py_example_*" commands to run the examples!')
 
@@ -48,6 +51,57 @@ def PyExampleMyLogicalEntity(args):
     eventqueue.AddEvent(mylogicalent, "Tick", value, 0.5, None, None)
     eventqueue.AddEvent(mylogicalent, "Tick", value, 0.75, None, None)
     eventqueue.AddEvent(mylogicalent, "Tick", value, 1.0, None, None)
+    
+    
+@concommand('py_example_mymodelentity')
+def PyExampleMyModelEntity(args):
+    # Register our entity factories by importing the module containing them (if not already imported)
+    print('Creating entity classes and factories...')
+    from . import entity
+
+    # Spawn model entity
+    forward = Vector()
+    player = UTIL_GetCommandClient()
+ 
+    print('Spawning my_model_entity')
+    AngleVectors(player.EyeAngles(), forward)
+    modelent = CreateEntityByName("my_model_entity")
+    origin = player.GetAbsOrigin() + forward * 256 + Vector(0,0,64)
+    angles = QAngle(0, player.GetAbsAngles().y - 90, 0)
+    modelent.SetAbsOrigin(origin)
+    modelent.SetAbsAngles(angles)
+    modelent.SetName('my_model_entity')
+    DispatchSpawn(modelent)
+    
+    # Activate MoveThink example
+    engine.ClientCommand(player, 'ent_fire my_model_entity Toggle')
+    
+@concommand('py_example_mymodelentity_removeall', helpstring='Removes all active my_model_entity entities')
+def PyExampleMyModelEntityRemoveAll(args):
+    modelent = entlist.FindEntityByClassname(None, 'my_model_entity')
+    while modelent:
+        modelentinst = modelent.Get()
+        print('model ent handle: %s, model ent instance: %s. Removing...' % (modelent, modelentinst))
+        UTIL_RemoveImmediate(modelent)
+        
+        # After removal the handle is None
+        # The reference to instance will still be valid
+        # The class instance of the reference is rebound to an empty class to prevent access to potential dangerous methods
+        try:
+            print(modelent.GetEntityName())
+        except AttributeError:
+            print('EXAMPLE EXCEPTION: Attempted to access None, because handle went None after removing the entity.')
+            traceback.print_exc()
+            
+        try:
+            print('EXAMPLE EXCEPTION: GetEntityName not found, because the class on the entity changed')
+            print(modelentinst.GetEntityName())
+        except AttributeError:
+            traceback.print_exc()
+        
+        # Find next
+        modelent = entlist.FindEntityByClassname(None, 'my_model_entity')
+    
     
 @concommand('py_example_networkedentity')
 def PyExampleNetworkedEntity(args):
--- mp/game/pysource/python/examples/entity.py
+++ mp/game/pysource/python/examples/entity.py
@@ -1,5 +1,9 @@
-from entities import CBaseEntity, CLogicalEntity, entity
-from game.fields import IntegerField, OutputField, input
+from vmath import Vector, QAngle, vec3_origin, VectorAngles, RandomVector
+from entities import CBaseEntity, CLogicalEntity, CBaseAnimating, entity, SOLID_BBOX, MOVETYPE_FLY, MOVETYPE_NONE
+from utils import UTIL_SetSize
+from game.fields import IntegerField, BooleanField, FloatField, OutputField, input
+
+import random
 
 # Stripped down example of creating a new entity
 # The entity class must be derived from BaseEntity
@@ -35,4 +39,70 @@ class CMyLogicalEntity(CLogicalEntity):
             # Reset our counter
             self.counter = 0
         
+# Python example of https://developer.valvesoftware.com/wiki/Authoring_a_Model_Entity
+@entity('my_model_entity')
+class CMyModelEntity(CBaseAnimating):
+    # Name of our entity's model
+    ENTITY_MODEL = "models/gibs/airboat_broken_engine.mdl"
+    
+    active = BooleanField(value=False)
+    nextchangetime = FloatField(value=0.0)
+
+    def Spawn(self):
+        self.Precache()
+     
+        self.SetModel(self.ENTITY_MODEL)
+        self.SetSolid(SOLID_BBOX)
+        UTIL_SetSize(self, -Vector(20,20,20), Vector(20,20,20))
         
+    def Precache(self):
+        self.PrecacheModel(self.ENTITY_MODEL)
+     
+        super(CMyModelEntity, self).Precache()
+    
+    def MoveThink(self):
+        # See if we should change direction again
+        if self.nextchangetime < gpGlobals.curtime:
+            # Randomly take a new direction and speed
+            newvelocity = RandomVector(-64.0, 64.0)
+            self.SetAbsVelocity(newvelocity)
+     
+            # Randomly change it again within one to three seconds
+            self.nextchangetime = gpGlobals.curtime + random.uniform(1.0, 3.0)
+     
+        # Snap our facing to where we're heading
+        vecfacing = self.GetAbsVelocity()
+        angfacing = QAngle()
+        VectorAngles(vecfacing, angfacing)
+        self.SetAbsAngles(angfacing)
+     
+        # Think at 20Hz
+        self.SetNextThink(gpGlobals.curtime + 0.05)
+
+    @input('Toggle')
+    def InputToggle(self, inputdata):
+        # Toggle our active state
+        if not self.active:
+            # Start thinking
+            self.SetThink(self.MoveThink)
+     
+            self.SetNextThink(gpGlobals.curtime + 0.05)
+     
+            # Start moving
+            self.SetMoveType(MOVETYPE_FLY)
+     
+            # Force MoveThink() to choose a new speed and direction immediately
+            self.nextchangetime = gpGlobals.curtime
+     
+            # Update self.active to reflect our new state
+            self.active = True
+        else:
+            # Stop thinking
+            self.SetThink(None)
+     
+            # Stop moving
+            self.SetAbsVelocity(vec3_origin)
+            self.SetMoveType(MOVETYPE_NONE)
+     
+            self.active = False
+    
