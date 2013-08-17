084f3d31a890ad3d5805c9e3c8dce3b6565e2fe9
Sandern <vannoortsander@gmail.com>
2013-08-17 15:30:55 +0200

    Added comments to usermessages example

--- mp/game/pysource/python/examples/usermessages.py
+++ mp/game/pysource/python/examples/usermessages.py
@@ -1,10 +1,17 @@
 from game.usermessages import usermessage, usermessage_shared
 
+# The decorator usermessage turns a method into a usermessage method
+# Calling this on the server will result in the method being called on client(s)
+# An optional filter keyword argument can be used to control on which clients the method is called
+# By default, a CReliableBroadcastRecipientFilter is used
+# Internally this uses gameinterface.SendUserMessage, which wraps around UserMessageBegin/MessageEnd
+# The arguments types you can send are limited to int, float, str, bool, none, set, dict, list, Vector, QAngle and entity handles.
 @usermessage()
 def PyExampleUserMessage(intdata, listdata, dictdata, *args, **kwargs):
     print('%s: PyExampleUserMessage received intdata %s, listdata %s and dictdata %s!' % (
             'Client' if isclient else 'Server', intdata, listdata, dictdata))
             
+# usermessage_shared is the same as above, but also calls the method on the server
 @usermessage_shared()
 def PyExampleUserMessageShared(intdata, listdata, dictdata, *args, **kwargs):
     print('%s: PyExampleUserMessage received intdata %s, listdata %s and dictdata %s!' % (
