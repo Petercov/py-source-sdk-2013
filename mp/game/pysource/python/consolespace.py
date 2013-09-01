f314e5031e2d967df2b174642f4595856d8651d4
Sandern <vannoortsander@gmail.com>
2013-09-01 19:50:03 +0200

    Added "player" attribute to consolespace module, which can be used in the spy/cpy commands. The player attribute is an handle to the first player in the game.

--- mp/game/pysource/python/consolespace.py
+++ mp/game/pysource/python/consolespace.py
@@ -7,7 +7,23 @@ import os
 import sys
 from imp import reload
 
+from game.dispatch import receiver
 import entities
-from entities import entlist
+from entities import entlist, CBasePlayer
 from utils import *
 
+# Set variables
+if isserver:
+    from game.signals import clientactive
+    @receiver(clientactive)
+    def __ClientActive(sender, client, **kwargs):
+        global player
+        if client.entindex() == 1:
+            player = client
+            
+else:
+    from game.signals import postlevelinit
+    @receiver(postlevelinit)
+    def LevelInit(sender, **kwargs): 
+        global player
+        player = CBasePlayer.GetLocalPlayer()
