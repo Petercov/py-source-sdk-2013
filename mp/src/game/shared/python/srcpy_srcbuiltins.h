a869071f04b625456a1e412b575742d9d6acbc03
Sandern <vannoortsander@gmail.com>
2013-12-05 20:38:36 +0100

    Fixed potential security/crash problem in the print redirector for Python.

--- mp/src/game/shared/python/srcpy_srcbuiltins.h
+++ mp/src/game/shared/python/srcpy_srcbuiltins.h
@@ -17,20 +17,20 @@
 class SrcPyStdOut 
 {
 public:
-	void write( const char *msg ) { Msg( msg ); }
+	void write( const char *msg ) { Msg( "%s", msg ); }
 	void flush() {}
 };
 
 class SrcPyStdErr 
 {
 public:
-	void write( const char *msg ) { Warning( msg ); }
+	void write( const char *msg ) { Warning( "%s", msg ); }
 	void flush() {}
 };
 
 // Wrappers for Msg, Warning and DevMsg (Python does not use VarArgs)
-inline void SrcPyMsg( const char *msg ) { Msg( msg ); }
-inline void SrcPyWarning( const char *msg ) { Warning( msg ); }
-inline void SrcPyDevMsg( int level, const char *msg ) { DevMsg( level, msg ); }
+inline void SrcPyMsg( const char *msg ) { Msg( "%s", msg ); }
+inline void SrcPyWarning( const char *msg ) { Warning( "%s", msg ); }
+inline void SrcPyDevMsg( int level, const char *msg ) { DevMsg( level, "%s", msg ); }
 
 #endif // SRCPY_SRCBUILTINS_H
