48a8d60dcaaffc5f4a1408d75f058692c53013c4
Sander van Noort <vannoortsander@gmail.com>
2014-11-16 09:46:44 +0100

    Update module config list for osx

--- mp/src/game/shared/python/srcpy_config.cpp
+++ mp/src/game/shared/python/srcpy_config.cpp
@@ -1,6 +1,8 @@
-//====== Copyright © Sandern Corporation, All rights reserved. ===========//
+//====== Copyright ï¿½ Sandern Corporation, All rights reserved. ===========//
 //
 // Purpose: Controls the available core Python modules
+//
+// TOOD: Remove unneeded modules or maybe don't use this at all.
 //
 // $NoKeywords: $
 //=============================================================================//
@@ -17,7 +19,8 @@
 /* This file contains the table of built-in modules.
    See init_builtin() in import.c. */
 
-extern "C" 
+#ifdef WIN32
+extern "C"
 {
 
 PyMODINIT_FUNC PyInit_array(void);
@@ -181,3 +184,134 @@ struct _inittab _PySourceImport_Inittab[] = {
     /* Sentinel */
     {0, 0}
 };
+
+#endif // WIN32
+
+#ifdef OSX
+
+extern "C" {
+	
+	
+	extern PyObject* PyInit__thread(void);
+	extern PyObject* PyInit_signal(void);
+	extern PyObject* PyInit_posix(void);
+	extern PyObject* PyInit_errno(void);
+	extern PyObject* PyInit_pwd(void);
+	extern PyObject* PyInit__sre(void);
+	extern PyObject* PyInit__codecs(void);
+	extern PyObject* PyInit__weakref(void);
+	extern PyObject* PyInit__functools(void);
+	extern PyObject* PyInit__operator(void);
+	extern PyObject* PyInit__collections(void);
+	extern PyObject* PyInit_itertools(void);
+	extern PyObject* PyInit_atexit(void);
+	extern PyObject* PyInit__stat(void);
+	extern PyObject* PyInit__locale(void);
+	extern PyObject* PyInit__io(void);
+	extern PyObject* PyInit_zipimport(void);
+	extern PyObject* PyInit_faulthandler(void);
+	extern PyObject* PyInit__tracemalloc(void);
+	extern PyObject* PyInit__symtable(void);
+	extern PyObject* PyInit_array(void);
+	extern PyObject* PyInit_cmath(void);
+	extern PyObject* PyInit_math(void);
+	extern PyObject* PyInit__struct(void);
+	extern PyObject* PyInit_time(void);
+	extern PyObject* PyInit__weakref(void);
+	extern PyObject* PyInit__random(void);
+	extern PyObject* PyInit__elementtree(void);
+	extern PyObject* PyInit__pickle(void);
+	extern PyObject* PyInit__datetime(void);
+	extern PyObject* PyInit__bisect(void);
+	extern PyObject* PyInit__heapq(void);
+	extern PyObject* PyInit_unicodedata(void);
+	extern PyObject* PyInit__socket(void);
+	extern PyObject* PyInit__md5(void);
+	extern PyObject* PyInit__sha1(void);
+	extern PyObject* PyInit__sha256(void);
+	extern PyObject* PyInit__sha512(void);
+	extern PyObject* PyInit_binascii(void);
+	extern PyObject* PyInit_parser(void);
+	extern PyObject* PyInit_xxsubtype(void);
+	/* -- ADDMODULE MARKER 1 -- */
+	
+	extern PyObject* PyMarshal_Init(void);
+	extern PyObject* PyInit_imp(void);
+	extern PyObject* PyInit_gc(void);
+	extern PyObject* PyInit__ast(void);
+	extern PyObject* _PyWarnings_Init(void);
+	extern PyObject* PyInit__string(void);
+	
+	struct _inittab _PySourceImport_Inittab[] = {
+		
+		{"_thread", PyInit__thread},
+		{"signal", PyInit_signal},
+		{"posix", PyInit_posix},
+		{"errno", PyInit_errno},
+		{"pwd", PyInit_pwd},
+		{"_sre", PyInit__sre},
+		{"_codecs", PyInit__codecs},
+		{"_weakref", PyInit__weakref},
+		{"_functools", PyInit__functools},
+		{"_operator", PyInit__operator},
+		{"_collections", PyInit__collections},
+		{"itertools", PyInit_itertools},
+		{"atexit", PyInit_atexit},
+		{"_stat", PyInit__stat},
+		{"_locale", PyInit__locale},
+		{"_io", PyInit__io},
+		{"zipimport", PyInit_zipimport},
+		{"faulthandler", PyInit_faulthandler},
+		{"_tracemalloc", PyInit__tracemalloc},
+		{"_symtable", PyInit__symtable},
+		{"array", PyInit_array},
+		{"cmath", PyInit_cmath},
+		{"math", PyInit_math},
+		{"_struct", PyInit__struct},
+		{"time", PyInit_time},
+		{"_weakref", PyInit__weakref},
+		{"_random", PyInit__random},
+		{"_elementtree", PyInit__elementtree},
+		{"_pickle", PyInit__pickle},
+		{"_datetime", PyInit__datetime},
+		{"_bisect", PyInit__bisect},
+		{"_heapq", PyInit__heapq},
+		{"unicodedata", PyInit_unicodedata},
+		{"_socket", PyInit__socket},
+		{"_md5", PyInit__md5},
+		{"_sha1", PyInit__sha1},
+		{"_sha256", PyInit__sha256},
+		{"_sha512", PyInit__sha512},
+		{"binascii", PyInit_binascii},
+		{"parser", PyInit_parser},
+		{"xxsubtype", PyInit_xxsubtype},
+		/* -- ADDMODULE MARKER 2 -- */
+		
+		/* This module lives in marshal.c */
+		{"marshal", PyMarshal_Init},
+		
+		/* This lives in import.c */
+		{"_imp", PyInit_imp},
+		
+		/* This lives in Python/Python-ast.c */
+		{"_ast", PyInit__ast},
+		
+		/* These entries are here for sys.builtin_module_names */
+		{"builtins", NULL},
+		{"sys", NULL},
+		
+		/* This lives in gcmodule.c */
+		{"gc", PyInit_gc},
+		
+		/* This lives in _warnings.c */
+		{"_warnings", _PyWarnings_Init},
+		
+		/* This lives in Objects/unicodeobject.c */
+		{"_string", PyInit__string},
+		
+		/* Sentinel */
+		{0, 0}
+	};
+}
+
+#endif // OSX
